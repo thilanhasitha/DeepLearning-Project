7d311616711f062cd424363e2116d2e6caba8829
Thilan Hasitha <156594121+thilanhasitha@users.noreply.github.com>
2025-07-03 16:56:06 +0530

    setup file updated

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages,setup
+
+def get_requirements(file_path:str)->list[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements=[req.replace("\n","")for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+setup(
+
+    name="Xray",
+    version="0.0.1",
+    author = "thilan hasitha",
+    author_email = "yasanjithbgth@gmail.com",
+    install_requires =get_requirements(),
+    packages=find_packages(),
+)
